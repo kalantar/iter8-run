70d8b66a0c5af955f89b092a0745f57905b2b0c8
Michael Kalantar <kalantar@us.ibm.com>
2021-11-02 16:22:39 -0400

    input experiment
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,27 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-echo "Starting Experiment..."
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Creating working directory"
+
+WORK_DIR=`mktemp -d -p  "$DIR"`
+if [[ ! "$WORK_DIR" || ! -d  "$WORK_DIR" ]]; then
+  echo "Cound not create temporary working directory"
+  exit 1
+fi
+
+# no need to cleanup
+
+echo "Copying experiment definition"
+mv "${1}" ${WORK_DIR}/experiment.yaml
+cd ${WORK_DIR}
+
+echo "Running Experiment"
 /bin/iter8 run
+
 echo "Run completed; verifying completeness"
 /bin/iter8 assert -c completed -c noFailure
-echo $?
+rc=$?
+
+echo "Return code: $rc"
+exit $rc
