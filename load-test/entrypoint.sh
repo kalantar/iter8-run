f71adf5eb528ddc075e140d05037d63fa7dcd315
Michael Kalantar <kalantar@us.ibm.com>
2022-01-20 15:06:03 -0500

    use --set
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- load-test/entrypoint.sh
+++ load-test/entrypoint.sh
@@ -19,59 +19,54 @@ echo "Fetch load-test experiment"
 $ITER8 hub -e load-test
 cd load-test
 
+SETS=""
 echo "Modify experiment using inputs"
 if [[ ! -z "${INPUT_URL}" ]]; then
-  yq eval -i ".url = \"${INPUT_URL}\"" values.yaml
+  SETS="$SETS --set url=\"${INPUT_URL}\""
 fi
 if [[ ! -z "${INPUT_NUMQUERIES}" ]]; then
-  yq eval -i ".numQueries = \"${INPUT_NUMQUERIES}\"" values.yaml
+  SETS="$SETS --set numQueries=\"${INPUT_NUMQUERIES}\""
 fi
-if [[ ! -z "${INPUT_DURATION}" ]]; then
-  yq eval -i ".duration = \"${INPUT_DURATION}\"" values.yaml
-fi
-if [[ ! -z "${INPUT_QPS}" ]]; then
-  yq eval -i ".qps = \"${QPS}\"" values.yaml
-fi
-if [[ ! -z "${INPUT_CONNECTIONS}" ]]; then
-  yq eval -i ".connections = \"${INPUT_CONNECTIONS}\"" values.yaml
-fi
-if [[ ! -z "${INPUT_PAYLOADSTR}" ]]; then
-  yq eval -i ".payloadStr = \"${INPUT_PAYLOADSTR}\"" values.yaml
-fi
-if [[ ! -z "${INPUT_PAYLOADURL}" ]]; then
-  yq eval -i ".payloadUrl = \"${INPUT_PAYLOADURL}\"" values.yaml
-fi
-if [[ ! -z "${INPUT_CONTENTTYPE}" ]]; then
-  yq eval -i ".contentType = \"${INPUT_CONTENTTYPE}\"" values.yaml
-fi
-if [[ ! -z "${INPUT_ERRORRANGES}" ]]; then
-  yq eval -i ".errorRanges = \"${INPUT_ERRORRANGES}\"" values.yaml
-fi
-if [[ ! -z "${INPUT_PERCENTILES}" ]]; then
-  yq eval -i ".percentiles = \"${INPUT_PERCENTILES}\"" values.yaml
-fi
+# if [[ ! -z "${INPUT_DURATION}" ]]; then
+#   yq eval -i ".duration = \"${INPUT_DURATION}\"" values.yaml
+# fi
+# if [[ ! -z "${INPUT_QPS}" ]]; then
+#   yq eval -i ".qps = \"${QPS}\"" values.yaml
+# fi
+# if [[ ! -z "${INPUT_CONNECTIONS}" ]]; then
+#   yq eval -i ".connections = \"${INPUT_CONNECTIONS}\"" values.yaml
+# fi
+# if [[ ! -z "${INPUT_PAYLOADSTR}" ]]; then
+#   yq eval -i ".payloadStr = \"${INPUT_PAYLOADSTR}\"" values.yaml
+# fi
+# if [[ ! -z "${INPUT_PAYLOADURL}" ]]; then
+#   yq eval -i ".payloadUrl = \"${INPUT_PAYLOADURL}\"" values.yaml
+# fi
+# if [[ ! -z "${INPUT_CONTENTTYPE}" ]]; then
+#   yq eval -i ".contentType = \"${INPUT_CONTENTTYPE}\"" values.yaml
+# fi
+# if [[ ! -z "${INPUT_ERRORRANGES}" ]]; then
+#   yq eval -i ".errorRanges = \"${INPUT_ERRORRANGES}\"" values.yaml
+# fi
+# if [[ ! -z "${INPUT_PERCENTILES}" ]]; then
+#   yq eval -i ".percentiles = \"${INPUT_PERCENTILES}\"" values.yaml
+# fi
 if [[ ! -z "${INPUT_ERROR_RATE}" ]]; then
-  yq eval -i ".error-rate = \"${INPUT_ERROR_RATE}\"" values.yaml
+  SETS="$SETS --set SLOs.error-rate=\"${INPUT_ERROR_RATE}\""
 fi
 if [[ ! -z "${INPUT_MEAN_LATENCY}" ]]; then
-  yq eval -i ".mean-latency = \"${INPUT_MEAN_LATENCY}\"" values.yaml
+  SETS="$SETS --set SLOs.mean-latency=\"${INPUT_MEAN_LATENCY}\""
 fi
 if [[ ! -z "${INPUT_P95_0}" ]]; then
-  yq eval -i ".p\'95\.0\' = \"${INPUT_P95_0}\"" values.yaml
+  SETS="$SETS --set SLOs.p95=\"${INPUT_P95_0}\""
 fi
 
-echo ">>>>>>>>>>>>>>>>>>>>>"
-echo ">>>>>     values.yaml"
-echo ">>>>>>>>>>>>>>>>>>>>>"
-cat values.yaml
-$ITER8 run --dry
-echo ">>>>>>>>>>>>>>>>>>>>>"
-echo ">>>>> experiment.yaml"
-echo ">>>>>>>>>>>>>>>>>>>>>"
+"Create experiment.yaml for inspection"
+$ITER8 run --dry $SETS
 cat experiment.yaml
 
 echo "Run Experiment"
-LOG_LEVEL=trace $ITER8 run
+LOG_LEVEL=trace $ITER8 run $SETS
 
 echo "Log result"
 $ITER8 report
