64bb45d0063f24ed4e3a575b382ea99b8e67a552
Michael Kalantar <kalantar@us.ibm.com>
2022-01-21 11:00:45 -0500

    remove errorRange and percentiles
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- load-test/entrypoint.sh
+++ load-test/entrypoint.sh
@@ -19,73 +19,74 @@ echo "Fetch load-test experiment"
 $ITER8 hub -e load-test
 cd load-test
 
-SETS=""
+OPTIONS=""
 echo "Modify experiment using inputs"
 if [[ ! -z "${INPUT_URL}" ]]; then
-  SETS="$SETS --set url=${INPUT_URL}"
+  OPTIONS="$OPTIONS --set url=${INPUT_URL}"
 fi
-
 if [[ ! -z "${INPUT_NUMQUERIES}" ]]; then
-  SETS="$SETS --set numQueries=${INPUT_NUMQUERIES}"
+  OPTIONS="$OPTIONS --set numQueries=${INPUT_NUMQUERIES}"
 fi
 if [[ ! -z "${INPUT_DURATION}" ]]; then
-  SETS="$SETS --set duration=${INPUT_DURATION}"
+  OPTIONS="$OPTIONS --set duration=${INPUT_DURATION}"
 fi
 if [[ ! -z "${INPUT_QPS}" ]]; then
-  SETS="$SETS --set qps=${INPUT_QPS}"
+  OPTIONS="$OPTIONS --set qps=${INPUT_QPS}"
 fi
 if [[ ! -z "${INPUT_CONNECTIONS}" ]]; then
-  SETS="$SETS --set connections=${INPUT_CONNECTIONS}"
+  OPTIONS="$OPTIONS --set connections=${INPUT_CONNECTIONS}"
 fi
-
 if [[ ! -z "${INPUT_PAYLOADSTR}" ]]; then
-  SETS="$SETS --set payloadStr=\"${INPUT_PAYLOADSTR}\""
+  OPTIONS="$OPTIONS --set payloadStr=\"${INPUT_PAYLOADSTR}\""
 fi
 if [[ ! -z "${INPUT_PAYLOADURL}" ]]; then
-  SETS="$SETS --set payloadUrl=\"${INPUT_PAYLOADURL}\""
+  OPTIONS="$OPTIONS --set payloadUrl=\"${INPUT_PAYLOADURL}\""
 fi
 if [[ ! -z "${INPUT_CONTENTTYPE}" ]]; then
-  SETS="$SETS --set contentType=\"${INPUT_CONTENTTYPE}\""
+  OPTIONS="$OPTIONS --set contentType=\"${INPUT_CONTENTTYPE}\""
 fi
-
 if [[ ! -z "${INPUT_ERROR_RATE}" ]]; then
-  SETS="$SETS --set SLOs.error-rate=${INPUT_ERROR_RATE}"
+  OPTIONS="$OPTIONS --set SLOs.error-rate=${INPUT_ERROR_RATE}"
 fi
 if [[ ! -z "${INPUT_MEAN_LATENCY}" ]]; then
-  SETS="$SETS --set SLOs.mean-latency=${INPUT_MEAN_LATENCY}"
+  OPTIONS="$OPTIONS --set SLOs.mean-latency=${INPUT_MEAN_LATENCY}"
 fi
 if [[ ! -z "${INPUT_P25}" ]]; then
-  SETS="$SETS --set SLOs.p25=${INPUT_P25}"
+  OPTIONS="$OPTIONS --set SLOs.p25=${INPUT_P25}"
 fi
 if [[ ! -z "${INPUT_P50}" ]]; then
-  SETS="$SETS --set SLOs.p50=${INPUT_P50}"
+  OPTIONS="$OPTIONS --set SLOs.p50=${INPUT_P50}"
 fi
 if [[ ! -z "${INPUT_P75}" ]]; then
-  SETS="$SETS --set SLOs.p75=${INPUT_P75}"
+  OPTIONS="$OPTIONS --set SLOs.p75=${INPUT_P75}"
 fi
 if [[ ! -z "${INPUT_P90}" ]]; then
-  SETS="$SETS --set SLOs.p90=${INPUT_P90}"
+  OPTIONS="$OPTIONS --set SLOs.p90=${INPUT_P90}"
 fi
 if [[ ! -z "${INPUT_P95}" ]]; then
-  SETS="$SETS --set SLOs.p95=${INPUT_P95}"
+  OPTIONS="$OPTIONS --set SLOs.p95=${INPUT_P95}"
 fi
 if [[ ! -z "${INPUT_P97_5}" ]]; then
-  SETS="$SETS --set SLOs.p\'97\.5'=${INPUT_P97_5}"
+  OPTIONS="$OPTIONS --set SLOs.p\'97\.5'=${INPUT_P97_5}"
 fi
 if [[ ! -z "${INPUT_P99}" ]]; then
-  SETS="$SETS --set SLOs.p99=${INPUT_P99}"
+  OPTIONS="$OPTIONS --set SLOs.p99=${INPUT_P99}"
 fi
 if [[ ! -z "${INPUT_P99_9}" ]]; then
-  SETS="$SETS --set SLOs.p\'99\.9\'=${INPUT_P99_9}"
+  OPTIONS="$OPTIONS --set SLOs.p\'99\.9\'=${INPUT_P99_9}"
+fi
+
+if [[ ! -z "${INPUT_VALUES}" ]]; then
+  OPTIONS="$OPTIONS -f ${INPUT_VALUES}"
 fi
 
 echo "Create experiment.yaml for inspection"
-echo $SETS
-$ITER8 run --dry $SETS
+echo "$ITER8 run --dry $OPTIONS"
+$ITER8 run --dry $OPTIONS
 cat experiment.yaml
 
 echo "Run Experiment"
-LOG_LEVEL=trace $ITER8 run $SETS
+LOG_LEVEL=trace $ITER8 run $OPTIONS
 
 echo "Log result"
 $ITER8 report
