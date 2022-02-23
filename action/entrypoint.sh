732c30e760c44e0fc14542b1601bcc0789e6d549
Michael Kalantar <kalantar@us.ibm.com>
2022-02-23 10:19:56 -0500

    add validateSLOs
    Signed-off-by: Michael Kalantar <kalantar@us.ibm.com>

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -18,28 +18,30 @@ fi
 # set LOG_LEVEL for iter8 commands
 export LOG_LEVEL="${INPUT_LOGLEVEL}"
 
-echo "Fetch experiment"
-$ITER8 hub -e ${INPUT_CHART}
-cd $(basename ${INPUT_CHART})
+# echo "Fetch experiment"
+# $ITER8 hub -e ${INPUT_CHART}
+# cd $(basename ${INPUT_CHART})
 
 echo "Identify values file"
 OPTIONS=""
 if [[ ! -z "${INPUT_VALUESFILE}" ]]; then
-  OPTIONS="$OPTIONS -f values.yaml -f ../${INPUT_VALUESFILE}"
+  OPTIONS="$OPTIONS -f ${INPUT_VALUESFILE}"
 fi
 
-echo "Create experiment.yaml for inspection"
-echo "$ITER8 run --dry $OPTIONS"
-$ITER8 run --dry $OPTIONS
-cat experiment.yaml
+$ITER8 launch -c ${INPUT_CHART} ${OPTIONS}
 
-echo "Run Experiment"
-$ITER8 run $OPTIONS
+# echo "Create experiment.yaml for inspection"
+# echo "$ITER8 run --dry $OPTIONS"
+# $ITER8 run --dry $OPTIONS
+# cat experiment.yaml
+
+# echo "Run Experiment"
+# $ITER8 run $OPTIONS
 
 echo "Log result"
 $ITER8 report
 
-echo "Run completed; verifying completeness"
+echo "Experiment completed"
 # return 0 if satisfied; else non-zero
 if [[ "${INPUT_VALIDATESLOS}" == "true" ]]; then
   echo "Asserting SLOs satisfied"
